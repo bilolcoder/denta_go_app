86dfe152b981010b4c3b17c54f52fc7bf789c2f7
bilolcoder <bilolcoder2009@gmail.com>
2025-12-20 13:05:51 +0500

    savat

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { CartProvider } from './CartContext'
 import BottomNavbar from './Sitebar'
+import { CartProvider } from './CartContext' // 1. Providerni import qiling
+
+// Komponentlar
 import Kategoriyalar from './Qidiruv/Kategoriyalar'
 import KichikKategoriyalar from './Qidiruv/KichikKategoriyalar'
 import Mahsulotlar from './Qidiruv/Mahsulotlar'
@@ -10,17 +12,18 @@ import Savat from './Savat/Savat'
 
 function App() {
   return (
+    // 2. Hammasini CartProvider bilan o'rab chiqing
     <CartProvider>
       <div className="bg-white min-h-screen">
-        <div className="pb-24 pt-4">
+        <div className="px-4 lg:px-10 pb-24 pt-4">
           <Routes>
-            <Route path="/" element={<Kategoriyalar />} />
+            <Route path="/" element={<div>Bosh sahifa (Hali yaratilmagan)</div>} />
             <Route path="/kategoriyalar" element={<Kategoriyalar />} />
             <Route path="/kategoriyalar/:categoryKey" element={<KichikKategoriyalar />} />
             <Route path="/kategoriyalar/:categoryKey/:subKey" element={<Mahsulotlar />} />
             <Route path="/mahsulot/:categoryKey/:subKey/:productId" element={<MahsulotDetallar />} />
             <Route path="/savatcha" element={<Savat />} />
-            <Route path="/profil" element={<div className="p-10 text-center">Profil sahifasi</div>} />
+            <Route path="/profil" element={<div>Profil sahifasi</div>} />
           </Routes>
         </div>
         <BottomNavbar />
